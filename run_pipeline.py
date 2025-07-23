cc0070ed8d26f6192147984385ef4cc2141abf53
Pietro Curone <pcurone@das.uchile.cl>
2025-07-23 12:12:02 -0400

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,6 +12,7 @@ config = sys.argv[1]
 try:
     subprocess.run(["python", "build_model_image.py", config], check=True)
     subprocess.run(["python", "simulate_observation.py", config], check=True)
+    subprocess.run(["python", "recovery_score.py", config], check=True)
 except subprocess.CalledProcessError as e:
     print(f"\nPipeline stopped due to an error in: {e.cmd}")
     sys.exit(1)
